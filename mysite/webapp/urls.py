a484fceeea4ade984839cdebcbac9ae8b76db3fc
sangeetha <sangeetha.sn@inapp.com>
2018-07-27 19:05:00 +0530

    check_file

---
+++ mysite/webapp/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url,include
+from django.contrib import admin
+from webapp import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^webapp/$', include('webapp.urls')),
+    url(r'^hey/$', views.index, name='index'),
+    url(r'^admin/$', admin.site.urls),
+]
